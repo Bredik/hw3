9ab2853a7d2d160957a8c0ecfc39fbb86ee428c8
Bredik <br-bred@yandex.ru>
2024-07-09 15:31:42 +0300

    работает, но без ограничений и не быстрее

--- src/main/java/org/example/MultiThreadCrawler.java
+++ src/main/java/org/example/MultiThreadCrawler.java
@@ -2,7 +2,6 @@ package org.example;
 
 import lombok.AllArgsConstructor;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -10,6 +9,7 @@ import static java.lang.String.join;
 
 public class MultiThreadCrawler {
     private final ExecutorService executor = Executors.newFixedThreadPool(20);
+
     public static void main(String[] args) throws Exception {
         MultiThreadCrawler crawler = new MultiThreadCrawler();
 
@@ -17,10 +17,10 @@ public class MultiThreadCrawler {
         String result = crawler.find("British Empire", "Iron", 5, TimeUnit.MINUTES);
         long finishTime = TimeUnit.SECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
-        System.out.println("Took "+finishTime+" seconds, result is: " + result);
+        System.out.println("Took " + finishTime + " seconds, result is: " + result);
     }
 
-    private Queue<Node> searchQueue = new  ConcurrentLinkedQueue<>();
+    private Queue<Node> searchQueue = new ConcurrentLinkedQueue<>();
 
     private Set<String> visited = ConcurrentHashMap.newKeySet();
 
@@ -32,47 +32,56 @@ public class MultiThreadCrawler {
 
         Node resultAllPath = search(deadline, target);
 
-        if (resultAllPath != null) {
-            return getResult(resultAllPath);
+        if (resultAllPath.empty) {
+            return "not found";
         }
 
-        return "not found";
+        return getResult(resultAllPath);
     }
 
-    private Node search(long deadline, String target) throws TimeoutException, IOException {
-        Node result = null;
-        while (result == null && !searchQueue.isEmpty()) {
+    private Node search(long deadline, String target) throws TimeoutException {
+        while (!searchQueue.isEmpty()) {
             if (deadline < System.nanoTime()) {
                 throw new TimeoutException();
             }
 
-//            executor.submit(() -> {
-//
-//            });
+            var futures = new ArrayList<Future<Node>>();
 
-            Node node = searchQueue.poll();
-            System.out.println("Get page: " + node.title + " Thread: " + Thread.currentThread().getName());
-            Set<String> links = client.getByTitle(node.title);
-            if (links.isEmpty()) {
-                //pageNotFound
-                continue;
-            }
-            for (String link : links) {
-                String currentLink = link.toLowerCase();
-                if (visited.contains(currentLink)) {
-                    continue;
+            futures.add(executor.submit(() -> {
+                Node node = searchQueue.poll();
+                System.out.println("Get page: " + node.title + " Thread: " + Thread.currentThread().getName());
+                Set<String> links = client.getByTitle(node.title);
+                if (links.isEmpty()) {
+                    //pageNotFound
+                    return new Node();
                 }
-                visited.add(currentLink);
-                Node subNode = new Node(link, node);
-                if (target.equalsIgnoreCase(currentLink)) {
-                    result = subNode;
-                    continue;
+                for (String link : links) {
+                    String currentLink = link.toLowerCase();
+                    if (visited.add(currentLink)) {
+                        visited.add(currentLink);
+                        Node subNode = new Node(link, node);
+                        if (target.equalsIgnoreCase(currentLink)) {
+                            return subNode;
+                        }
+                        searchQueue.offer(subNode);
+                    }
                 }
-                searchQueue.offer(subNode);
+                return new Node();
+            }));
+
+            for (Future<Node> f : futures) {
+                try {
+                    if (!f.get().empty) {
+                        executor.shutdownNow();
+                        return f.get();
+                    }
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
             }
 
         }
-        return result;
+        return new Node();
     }
 
     private String getResult(Node result) {
@@ -90,9 +99,19 @@ public class MultiThreadCrawler {
         return join(" > ", resultList);
     }
 
-    @AllArgsConstructor
     private static class Node {
         String title;
         Node next;
+        boolean empty;
+
+        public Node() {
+            this.empty = true;
+        }
+
+        public Node(String title, Node next) {
+            this.title = title;
+            this.next = next;
+            this.empty = false;
+        }
     }
 }
