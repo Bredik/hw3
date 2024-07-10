e0746d0c2736ac102d7e721ceed76c9742594b4c
Bredik <br-bred@yandex.ru>
2024-07-10 09:07:46 +0300

    + глубина поиска

--- src/main/java/org/example/MultiThreadCrawler.java
+++ src/main/java/org/example/MultiThreadCrawler.java
@@ -1,20 +1,19 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-
 import java.util.*;
 import java.util.concurrent.*;
 
 import static java.lang.String.join;
 
 public class MultiThreadCrawler {
-    private final ExecutorService executor = Executors.newFixedThreadPool(20);
+    private final ExecutorService executor = Executors.newFixedThreadPool(2);
+    private final ExecutorService executor1 = Executors.newFixedThreadPool(2);
 
     public static void main(String[] args) throws Exception {
         MultiThreadCrawler crawler = new MultiThreadCrawler();
 
         long startTime = System.nanoTime();
-        String result = crawler.find("British Empire", "Iron", 5, TimeUnit.MINUTES);
+        String result = crawler.find("British Empire", "Iron", 2, 5, TimeUnit.MINUTES);
         long finishTime = TimeUnit.SECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
         System.out.println("Took " + finishTime + " seconds, result is: " + result);
@@ -26,11 +25,11 @@ public class MultiThreadCrawler {
 
     private WikiClient client = new WikiClient();
 
-    public String find(String from, String target, long timeout, TimeUnit timeUnit) throws Exception {
+    public String find(String from, String target, int deep, long timeout, TimeUnit timeUnit) throws Exception {
         long deadline = System.nanoTime() + timeUnit.toNanos(timeout);
         searchQueue.offer(new Node(from, null));
 
-        Node resultAllPath = search(deadline, target);
+        Node resultAllPath = search(deadline, target, deep);
 
         if (resultAllPath.empty) {
             return "not found";
@@ -39,13 +38,14 @@ public class MultiThreadCrawler {
         return getResult(resultAllPath);
     }
 
-    private Node search(long deadline, String target) throws TimeoutException {
+    private Node search(long deadline, String target, int deep) throws TimeoutException {
         while (!searchQueue.isEmpty()) {
             if (deadline < System.nanoTime()) {
                 throw new TimeoutException();
             }
 
             var futures = new ArrayList<Future<Node>>();
+            var chekDeep = new ArrayList<Future<Boolean>>();
 
             futures.add(executor.submit(() -> {
                 Node node = searchQueue.poll();
@@ -58,8 +58,8 @@ public class MultiThreadCrawler {
                 for (String link : links) {
                     String currentLink = link.toLowerCase();
                     if (visited.add(currentLink)) {
-                        visited.add(currentLink);
                         Node subNode = new Node(link, node);
+                        checkDeep(subNode, deep);
                         if (target.equalsIgnoreCase(currentLink)) {
                             return subNode;
                         }
@@ -97,6 +97,21 @@ public class MultiThreadCrawler {
         Collections.reverse(resultList);
 
         return join(" > ", resultList);
+    }
+
+    //Проверка глубина поиска по вложенности нод
+    private void checkDeep(Node n, int deep) {
+        int tmpDeep = 1;
+        var forPrint = n;
+        while (n.next != null) {
+            n = n.next;
+            tmpDeep++;
+        }
+        if (tmpDeep > deep) {
+            System.out.println(getResult(forPrint));
+            executor.shutdownNow();
+            throw new RuntimeException("Превысили глубину поиска");
+        }
     }
 
     private static class Node {
